b01929b8b3f7223accae4e763af06bae26cfb0d5
lumato92 <55516181+lumato92@users.noreply.github.com>
2022-06-14 20:02:12 -0300

    Update ejercicio2.py

--- Leccion_4/ejercicio2.py
+++ Leccion_4/ejercicio2.py
@@ -1,7 +1,12 @@
 numero_inicial=int(input("Ingrese Numero inicial: "))
 numero_final=int(input("Ingrese Numero final: "))
+resultado=[]
+
+while (numero_inicial > numero_final):
+	numero_final=int(input(f"El numero debe ser mayor a {numero_inicial}: "))
 
 for numero in range(numero_inicial,numero_final):
 	if numero%2 != 0:
-		print(numero)
-	
+		resultado.append(numero)
+
+print(resultado)
