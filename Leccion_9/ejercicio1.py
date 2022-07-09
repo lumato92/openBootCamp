7cd265fe8c1aadc3334c9b8ea1d0e3601b1d8d2a
lumato92 <55516181+lumato92@users.noreply.github.com>
2022-07-09 20:21:24 -0300

    leccion9 ejercicio1

---
+++ Leccion_9/ejercicio1.py
@@ -0,0 +1,9 @@
+
+# GENERO LISTA UTILIZO SPLIT PARA SEPARAR POR COMA(,) y luego hago un set para eliminar los elementos duplicados
+lista = set(input("Ingrese lista de paises separados por coma:").split(","))
+
+#Imprimo en consola los elementos del set de manera ordenada alfabeticamente
+
+print(sorted(lista))
+
+
